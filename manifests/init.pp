d750b1c376c8b70ba5556be4c02825fdded2a952
Mirosław Nawrocki <miroslaw.nawrocki@librus.pl>
2021-08-27 10:02:20 +0200

    Now works with Debian 9
    System version checking in package installation has been added.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,15 @@ class apachemodule {
     ensure => present
   }
 
-  package { 'apache2-prefork-dev':
-    require => Package['libaprutil1-dev'],
-    ensure  => present
+  if ($::operatingsystem == 'Ubuntu' and versioncmp($::operatingsystemrelease, '13.10') >= 0) or ($::operatingsystem == 'Debian' and versioncmp($::operatingsystemrelease, '8') >= 0) {
+    package { 'apache2-dev':
+      require => Package['libaprutil1-dev'],
+      ensure  => present
+    }
+  } else {
+    package { 'apache2-prefork-dev':
+      require => Package['libaprutil1-dev'],
+      ensure  => present
+    }
   }
 }
